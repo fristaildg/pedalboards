e149eb92d195bc404da095f25f57aea505158eb7
luisfernandezelectric <luis.fernandez@electric.ai>
2021-04-06 14:55:42 -0300

    uses router

--- pages/board/[id].tsx
+++ pages/board/[id].tsx
@@ -1,21 +1,28 @@
 import { useRouter } from 'next/router'
 import { PublicBoard } from '../../src/components/Board'
 
-const UserBoard = ({ id }) => {
+const UserBoard = () => {
+  const router = useRouter()
+  const { id } = router.query
+
+  console.log(id)
+
+  if (!id) return null
+
   return (
     <PublicBoard boardId={id} />
   )
 }
 
-export async function getStaticPaths() {
-  return { paths: [
-    {params: {id: 'sdqY0zLJRvKIWwTC2Sn2'}},
-    {params: {id: 'K1Cie0bldmnVRIOO8Ay4'}}
-  ], fallback: false }
-}
+// export async function getStaticPaths() {
+//   return { paths: [
+//     {params: {id: 'sdqY0zLJRvKIWwTC2Sn2'}},
+//     {params: {id: 'K1Cie0bldmnVRIOO8Ay4'}}
+//   ], fallback: false }
+// }
 
-export async function getStaticProps({ params }) {
-  return { props: { id: params.id }}
-}
+// export async function getStaticProps({ params }) {
+//   return { props: { id: params.id }}
+// }
 
 export default UserBoard
